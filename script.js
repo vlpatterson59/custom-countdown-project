3c3d78e2171ce1e9a14a385e7abc1e5f1ef8f1cb
Val Patterson <val@nutznboltz.website>
2021-06-18 14:05:24 -0400

    add event listener to form

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
 
+let countdownTitle = '';
+let countdownDate = '';
+
 // Set Date Input Min with Today's Date
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
+
+// Take Values from Form Input
+function updateCountdown(e) {
+    countdownTitle = e.srcElement[0].value;
+    countdownDate = e.srcElement[1].value;
+}
+
+// Add Listeners
+countdownForm.addEventListener('submit', updateCountdown);
